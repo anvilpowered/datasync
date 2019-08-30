fe8049621200b7ceee0ae745f3a076abb311f985
Alexander Staeding <alexanderstaeding@gmail.com>
2019-08-30 18:17:42 +0200

    Works for simpler items

--- sponge/src/main/java/rocks/milspecsg/msdatasync/service/implementation/data/MSInventorySerializer.java
+++ sponge/src/main/java/rocks/milspecsg/msdatasync/service/implementation/data/MSInventorySerializer.java
@@ -1,18 +1,22 @@
 package rocks.milspecsg.msdatasync.service.implementation.data;
 
 import com.google.common.collect.ImmutableList;
-import com.mongodb.BasicDBObject;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.data.DataContainer;
 import org.spongepowered.api.data.DataQuery;
 import org.spongepowered.api.data.DataView;
 import org.spongepowered.api.data.key.Key;
+import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.entity.living.player.User;
-import org.spongepowered.api.item.ItemType;
+import org.spongepowered.api.item.ItemTypes;
+import org.spongepowered.api.item.enchantment.Enchantment;
+import org.spongepowered.api.item.enchantment.EnchantmentTypes;
 import org.spongepowered.api.item.inventory.Inventory;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.scheduler.Task;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
 import rocks.milspecsg.msdatasync.MSDataSync;
 import rocks.milspecsg.msdatasync.model.core.Member;
 import rocks.milspecsg.msdatasync.model.core.SerializedItemStack;
@@ -22,6 +26,8 @@ import java.util.*;
 import java.util.concurrent.CompletableFuture;
 
 public class MSInventorySerializer extends ApiInventorySerializer<Member, Player, Key, User> {
+
+    private static char SEPARATOR = '_';
 
     @Override
     public CompletableFuture<Boolean> serialize(Member member, Player player) {
@@ -36,13 +42,32 @@ public class MSInventorySerializer extends ApiInventorySerializer<Member, Player
             return false;
         });
         Task.builder().execute(() -> {
+
             try {
                 List<SerializedItemStack> itemStacks = new ArrayList<>();
                 Sponge.getServer().getPlayer(player.getUniqueId()).ifPresent(p -> {
+                    p.getInventory().offer(
+                        ItemStack.builder()
+                            .itemType(ItemTypes.STICK)
+                            .add(Keys.DISPLAY_NAME, Text.of("This", TextColors.AQUA, Text.of(" is a stick", TextColors.BLUE)))
+                            .add(Keys.ITEM_LORE, Arrays.asList(Text.of("lore1", TextColors.BLACK), Text.of("lore2"), Text.of("lore3")))
+                            .add(Keys.ITEM_ENCHANTMENTS, Arrays.asList(
+                                Enchantment.builder()
+                                    .type(EnchantmentTypes.KNOCKBACK)
+                                    .level(50)
+                                    .build(),
+                                Enchantment.builder()
+                                    .type(EnchantmentTypes.SHARPNESS)
+                                    .level(30)
+                                    .build()
+                            ))
+                            .build()
+                    );
+
                     for (Inventory slot : p.getInventory().slots()) {
                         SerializedItemStack serializedItemStack = new SerializedItemStack();
-                        Map<DataQuery, Object> values = slot.peek().orElse(ItemStack.empty()).toContainer().getValues(false);
-                        serializedItemStack.properties = serialize(values);
+                        ItemStack before = slot.peek().orElse(ItemStack.empty());
+                        serializedItemStack.properties = serialize(before.toContainer().getValues(false));
                         itemStacks.add(serializedItemStack);
                     }
                 });
@@ -60,13 +85,13 @@ public class MSInventorySerializer extends ApiInventorySerializer<Member, Player
     private static Map<String, Object> serialize(Map<DataQuery, Object> values) {
         Map<String, Object> result = new HashMap<>();
         values.forEach((dq, o) -> {
-            String s = dq.asString("_");
+            String s = dq.asString(SEPARATOR);
 //            System.out.println("Class for " + s + ": " + o.getClass().getCanonicalName());
             if (o instanceof Map) {
                 Object m = serialize((Map<DataQuery, Object>) o);
 //                System.out.println("Finished (M), S: " + s + ", M: " + m.toString());
                 result.put(s, m);
-            } else if (o instanceof List) { // com.google.common.collect.SingletonImmutableList
+            } else if (o instanceof List) {
                 Object dc = serialize(((List<DataContainer>) o).get(0).getValues(false));
 //                System.out.println("Finished (DC), S: " + s + ", DC: " + dc.toString());
                 result.put(s, dc);
@@ -81,16 +106,20 @@ public class MSInventorySerializer extends ApiInventorySerializer<Member, Player
     private static Map<DataQuery, Object> deserialize(Map<String, Object> values) {
         Map<DataQuery, Object> result = new HashMap<>();
         values.forEach((s, o) -> {
-            DataQuery dq = DataQuery.of('_', s);
+            DataQuery dq = DataQuery.of(SEPARATOR, s);
             if (o instanceof Map) {
-                Map<String, Map<String, Object>> m = (Map<String, Map<String, Object>>) o;
-                Map<DataQuery, DataContainer> r1 = new HashMap<>();
+                Map<String, Object> m = (Map<String, Object>) o;
+                Map<DataQuery, Object> r1 = new HashMap<>();
                 m.forEach((s1, m1) -> {
-                    // convert each value to DataContainer
-                    Map<DataQuery, Object> v = deserialize(m1);
-                    DataContainer dc = DataContainer.createNew(DataView.SafetyMode.ALL_DATA_CLONED);
-                    v.forEach(dc::set);
-                    r1.put(DataQuery.of('_', s1), dc);
+                    Object value = m1;
+                    try {
+                        Map<DataQuery, Object> v = deserialize((Map<String, Object>) m1);
+                        DataContainer dc = DataContainer.createNew(DataView.SafetyMode.ALL_DATA_CLONED);
+                        v.forEach(dc::set);
+                        value = ImmutableList.of(dc);
+                    } catch (ClassCastException ignored) {
+                    }
+                    r1.put(DataQuery.of(SEPARATOR, s1), value);
                 });
                 result.put(dq, r1);
             } else {
@@ -104,7 +133,7 @@ public class MSInventorySerializer extends ApiInventorySerializer<Member, Player
     public CompletableFuture<Boolean> deserialize(Member member, Player player) {
         CompletableFuture<Boolean> future = CompletableFuture.supplyAsync(() -> {
             try {
-                // this really should not take longer than 30 seconds
+                // this really should not take longer than 10 seconds
                 // usually less than 1
                 Thread.sleep(10000);
             } catch (InterruptedException e) {
@@ -119,12 +148,9 @@ public class MSInventorySerializer extends ApiInventorySerializer<Member, Player
                     Iterator<Inventory> slots = p.getInventory().slots().iterator();
                     for (SerializedItemStack stack : member.itemStacks) {
                         if (slots.hasNext()) {
-                            DataContainer dc = DataContainer.createNew(DataView.SafetyMode.CLONED_ON_SET);
-
+                            DataContainer dc = DataContainer.createNew(DataView.SafetyMode.ALL_DATA_CLONED);
                             deserialize(stack.properties).forEach(dc::set);
-
                             ItemStack is = ItemStack.builder().fromContainer(dc).build();
-
                             slots.next().offer(is);
                         }
                     }
